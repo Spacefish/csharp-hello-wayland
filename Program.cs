9644b0b223d1309e5c241e07027470931a39952c
Timo Witte <timo.witte@gmail.com>
2025-03-30 13:34:21 +0200

    asd

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using WaylandSharp;
+
+var wlDisplay = WlDisplay.Connect();
+var wlRegistry = wlDisplay.GetRegistry();
+
+WlCompositor? wlCompositor = null;
+WlSeat? wlSeat = null;
+XdgWmBase? xdgWmBase = null;
+WlShm? wlShm = null;
+
+
+ZwpTextInputManagerV3? textInputManager = null;
+ZwpTextInputV3? textInput = null;
+
+
+/*
+wlDisplay.Error += (_, ea) =>
+{
+    Console.WriteLine($"WlDisplay Error: COde: {ea.Code} Message: {ea.Message}");
+};
+*/
+
+wlRegistry.Global += (_, ea) =>
+{
+    if(ea.Interface == WlInterface.WlCompositor.Name) {
+        wlCompositor = wlRegistry.Bind<WlCompositor>(ea.Name, ea.Interface, ea.Version);
+        Console.WriteLine($"Found: {ea.Name} {ea.Interface} {ea.Version}");
+    }
+    if(ea.Interface == WlInterface.XdgWmBase.Name) {
+        xdgWmBase = wlRegistry.Bind<XdgWmBase>(ea.Name, ea.Interface, ea.Version);
+        Console.WriteLine($"Found: {ea.Name} {ea.Interface} {ea.Version}");
+    }
+    if(ea.Interface == WlInterface.WlShm.Name) {
+        wlShm = wlRegistry.Bind<WlShm>(ea.Name, ea.Interface, ea.Version);
+        Console.WriteLine($"Found: {ea.Name} {ea.Interface} {ea.Version}");
+    }
+
+    if(ea.Interface == WlInterface.WlSeat.Name) {
+        wlSeat = wlRegistry.Bind<WlSeat>(ea.Name, ea.Interface, ea.Version);
+        Console.WriteLine($"Found: {ea.Name} {ea.Interface} {ea.Version}");
+    }
+    if(ea.Interface == WlInterface.ZwpTextInputManagerV3.Name) {
+        textInputManager = wlRegistry.Bind<ZwpTextInputManagerV3>(ea.Name, ea.Interface, ea.Version);
+        // tim.GetTextInput();
+        Console.WriteLine($"Found: {ea.Name} {ea.Interface} {ea.Version}");
+    }
+};
+wlDisplay.Roundtrip();
+
+if(wlCompositor == null)
+    throw new NotSupportedException($"Wayland registry did not advertise {WlInterface.WlCompositor.Name} found");
+if(xdgWmBase == null)
+    throw new NotSupportedException($"Wayland registry did not advertise {WlInterface.XdgWmBase.Name} found");
+if(wlShm == null)
+    throw new NotSupportedException($"Wayland registry did not advertise {WlInterface.WlShm.Name} found");
+
+if(wlSeat == null)
+    throw new NotSupportedException($"Wayland registry did not advertise {WlInterface.WlSeat.Name} found");
+if(textInputManager == null)
+    throw new NotSupportedException($"Wayland registry did not advertise {WlInterface.ZwpTextInputManagerV3.Name} found");
+
+
+xdgWmBase.Ping += (_, d) =>
+{
+    Console.WriteLine($"Ping: {d.Serial}");
+    xdgWmBase.Pong(d.Serial);
+};
+
+
+var mySurface = wlCompositor.CreateSurface();
+Console.WriteLine("Created surface!");
+
+var xdgSurface = xdgWmBase.GetXdgSurface(mySurface);
+xdgSurface.Configure += (_, d) =>
+{
+    Console.WriteLine($"Configure: {d.Serial}");
+    xdgSurface.AckConfigure(d.Serial);
+};
+Console.WriteLine("Created xdg surface!");
+
+var xdgToplevel = xdgSurface.GetToplevel();
+xdgToplevel.SetTitle("Hello World");
+xdgToplevel.SetAppId("com.example.helloworld");
+xdgToplevel.SetMaximized();
+xdgToplevel.SetMinSize(100, 100);
+// xdgToplevel.SetParent(null);
+
+
+mySurface.Commit();
+Console.WriteLine("Committed surface!");
+
+//var pool = wlShm.CreatePool(0, 200);
+//if(pool == null)
+    //throw new NullReferenceException("Failed to create pool");
+WlBuffer? wlBuffer = null;
+
+
+// mySurface.Attach();
+
+while(true) {
+    wlDisplay.Dispatch();
+    Thread.Sleep(100);
+    Console.WriteLine("Dispatching...");
+}
+
+
+textInput = textInputManager.GetTextInput(wlSeat)
+    ?? throw new NullReferenceException("Failed to get text input");
+
+textInput.CommitString += (_, d) => { Console.WriteLine($"CommitString: {d.Text}"); };
+textInput.DeleteSurroundingText += (_, d) => { Console.WriteLine($"DeleteSurroundingText: {d.BeforeLength} {d.AfterLength}"); };
+textInput.Done += (_, d) => { Console.WriteLine($"Done: {d.Serial}"); };
+textInput.Enter += (_, d) => { Console.WriteLine($"Enter: {d.Surface}"); };
+textInput.Leave += (_, d) => { Console.WriteLine($"Leave: {d.Surface}"); };
+textInput.PreeditString += (_, d) => { Console.WriteLine($"PreeditString: {d.Text} {d.CursorBegin} {d.CursorEnd}"); };
+textInput.Enable();
+
+// var tim = WlInterface.FromInterfaceName("zwp_text_input_manager_v3");
+
+Console.ReadLine();
+Console.WriteLine("HERE");
+// zwp_text_input_manager_v3
